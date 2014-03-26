05754df9610997948f9b7fe6576dc592f8c41e38
ailin chiu <achiu01@gmail.com>
2014-03-25 19:55:48 -0700

    test added

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def test_index
+		get :index 
+		assert_response :success
+		posts=assigns :posts
+		assert posts
+	end
+	def test_index_body
+		get :index
+		assert_match "Listing Posts", response.body 
+		puts response.body
+	end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+def test_title_len
+	post = Post.new
+	assert_not post.valid?
+end
+def test_valid_title_len
+	post = Post.new
+	post.title = "post name"
+	assert post.valid?
+end
+def test_valid_title_too_short
+	post = Post.new
+	post.title = ""
+	assert_not post.valid?
+end
 end
